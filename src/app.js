0752571c17bd188d3ca763a37c8d8b44a6b1e342
Emmanuel Lucena Montalvo <140103269+EmmanuelLM87@users.noreply.github.com>
2024-02-08 12:47:52 +0000

    añadido script app.js

--- src/app.js
+++ src/app.js
@@ -5,4 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {};
+window.onload = function() {
+  let pronombres = ["la", "nuestro", "el"];
+  let adjetivos = ["gran", "pequeño", "emocionante", "increible"];
+  let nombres = ["pony", "parque", "coche", "código"];
+  let tids = [".com", ".org", ".es", ".eu"];
+
+  let domainNames = [];
+
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let nombre of nombres) {
+        for (let tid of tids) {
+          domainNames.push(`${pronombre}${adjetivo}${nombre}${tid}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join(" ")}</ul>`;
+};
